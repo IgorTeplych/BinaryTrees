ef81d9f4671f7cb8b40e1af581fe8c544c3cc7e0
Igor Teplych <igorvrt@mail.ru>
2022-12-25 01:26:25 +0600

    Исправил метод удаления элемента из дерева. Возникала ошибка при удалении элемента где один из наследников или оба были null.

--- BinaryTrees.Tests/UnitTest1.cs
+++ BinaryTrees.Tests/UnitTest1.cs
@@ -39,7 +39,7 @@ namespace BinaryTrees.Tests
         }
 
         [Test]
-        public void RemoveInTrees()
+        public void RemoveNodeWithoutChild()
         {
             BinaryTrees.SimpleTrees simpleTrees = new BinaryTrees.SimpleTrees(10);
             simpleTrees.Insert(4);
@@ -63,6 +63,36 @@ namespace BinaryTrees.Tests
             Assert.AreEqual(simpleTrees.Search(4), false);
             Assert.AreEqual(simpleTrees.Search(12), true);
             Assert.AreEqual(simpleTrees.Search(14), true);
+            Assert.AreEqual(simpleTrees.Search(11), true);
+            Assert.AreEqual(simpleTrees.Search(18), true);
+            Assert.AreEqual(simpleTrees.Search(20), true);
+            Assert.AreEqual(simpleTrees.Search(102), true);
+        }
+        [Test]
+        public void RemoveNodeWithoutOneChild()
+        {
+            BinaryTrees.SimpleTrees simpleTrees = new BinaryTrees.SimpleTrees(10);
+            simpleTrees.Insert(4);
+            simpleTrees.Insert(12);
+            simpleTrees.Insert(14);
+            simpleTrees.Insert(11);
+            simpleTrees.Insert(18);
+            simpleTrees.Insert(20);
+            simpleTrees.Insert(102);
+
+            Assert.AreEqual(simpleTrees.Search(4), true);
+            Assert.AreEqual(simpleTrees.Search(12), true);
+            Assert.AreEqual(simpleTrees.Search(14), true);
+            Assert.AreEqual(simpleTrees.Search(11), true);
+            Assert.AreEqual(simpleTrees.Search(18), true);
+            Assert.AreEqual(simpleTrees.Search(20), true);
+            Assert.AreEqual(simpleTrees.Search(102), true);
+
+            simpleTrees.Remove(14);
+
+            Assert.AreEqual(simpleTrees.Search(4), true);
+            Assert.AreEqual(simpleTrees.Search(12), true);
+            Assert.AreEqual(simpleTrees.Search(14), false);
             Assert.AreEqual(simpleTrees.Search(11), true);
             Assert.AreEqual(simpleTrees.Search(18), true);
             Assert.AreEqual(simpleTrees.Search(20), true);
