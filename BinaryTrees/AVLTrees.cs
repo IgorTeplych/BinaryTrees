17c653a3649db561b73bcb44152cddedeaed3115
Igor Teplych <igorvrt@mail.ru>
2022-12-30 00:16:06 +0600

    Добавил реализацию АВЛ дерева

---
+++ BinaryTrees/AVLTrees.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTrees
+{
+    public class AVLTrees
+    {
+        Node _root;
+        public AVLTrees(int x)
+        {
+            _root = new Node();
+            _root.Key = x;
+            BalanceFactor = 2;
+            maxUnBalanceRoot = new Node();
+        }
+        /// <summary>
+        /// Коэффициент для балансировки дерева от 1 до max
+        /// </summary>
+        public int BalanceFactor { get; set; }
+        public Node Root
+        {
+            get
+            { return _root; }
+            set
+            {
+                _root = value;
+            }
+        }
+        public bool Search(int x)
+        {
+            return Search(x, _root);
+        }
+        bool Search(int x, Node node)
+        {
+            if (node == null)
+                return false;
+
+            if (node.Key == x)
+            {
+                return true;
+            }
+            else
+            {
+                if (x > node.Key)
+                {
+                    return Search(x, node.Right);
+                }
+                else
+                {
+                    return Search(x, node.Left);
+                }
+            }
+        }
+        Node SearchNode(int x, Node tree)
+        {
+            if (tree == null)
+                return null;
+
+            if (tree.Key == x)
+            {
+                return tree;
+            }
+            else
+            {
+                if (x > tree.Key)
+                {
+                    return SearchNode(x, tree.Right);
+                }
+                else
+                {
+                    return SearchNode(x, tree.Left);
+                }
+            }
+        }
+        public Node Insert(int x)
+        {
+            Insert(x, _root);
+            return _root;
+        }
+        void Insert(int x, Node node)
+        {
+            if (x > node.Key)
+            {
+                if (node.Right == null)
+                {
+                    Node newNode = new Node();
+                    newNode.Key = x;
+                    newNode.Parrent = node;
+                    node.Right = newNode;
+                }
+                else
+                {
+                    Insert(x, node.Right);
+                }
+            }
+            else
+            {
+                if (node.Left == null)
+                {
+                    Node newNode = new Node();
+                    newNode.Key = x;
+                    newNode.Parrent = node;
+                    node.Left = newNode;
+                }
+                else
+                {
+                    Insert(x, node.Left);
+                }
+            }
+        }
+        void Insert(Node insertNode, Node root)
+        {
+            if (insertNode.Key > root.Key)
+            {
+                if (root.Right == null)
+                {
+                    root.Right = insertNode;
+                    insertNode.Parrent = root;
+                }
+                else
+                {
+                    Insert(insertNode, root.Right);
+                }
+            }
+            else
+            {
+                if (root.Left == null)
+                {
+                    root.Left = insertNode;
+                    insertNode.Parrent = root;
+                }
+                else
+                {
+                    Insert(insertNode, root.Left);
+                }
+            }
+        }
+        public void Remove(int x)
+        {
+            Remove(x, _root);
+        }
+        void Remove(int x, Node node)
+        {
+            if (node == null) return;
+
+            if (x > node.Key)
+                Remove(x, node.Right);
+            else
+                Remove(x, node.Left);
+
+            if (node.Key == x)
+            {
+                Node node1 = node.Right;
+                Node node2 = node.Left;
+                Node _root = node.Parrent;
+
+                if (node.Key < node.Parrent.Key)
+                    node.Parrent.Left = null;
+                else
+                    node.Parrent.Right = null;
+
+                node = null;
+
+                if (node1 != null)
+                    Insert(node1, _root);
+                if (node2 != null)
+                    Insert(node2, _root);
+            }
+        }
+        public void SmallLeftRotation(Node tree)
+        {
+            Node root = tree;
+            tree.Right.Parrent = tree.Parrent;   //правой ветви назначаем нового отца
+
+            if (tree.Parrent.Right != null && tree.Parrent.Right.Key == tree.Key)
+                tree.Parrent.Right = tree.Right;    //Новому отцу сообщаем о новом потомке справа
+
+            if (tree.Parrent.Left != null && tree.Parrent.Left.Key == tree.Key)
+                tree.Parrent.Left = tree.Right; //Новому отцу сообщаем о новом потомке слева
+
+            tree = root.Right;
+            root.Right = null;
+            root.Parrent = null;
+            Insert(root, tree);
+        }
+        public void SmallRightRotation(Node tree)
+        {
+            Node root = tree;
+            tree.Left.Parrent = tree.Parrent;
+
+            if (tree.Parrent.Left != null && tree.Parrent.Left.Key == tree.Key)
+                tree.Parrent.Left = tree.Left;
+
+            if (tree.Parrent.Right != null && tree.Parrent.Right.Key == tree.Key)
+                tree.Parrent.Right = tree.Left;
+
+            tree = root.Left;
+            root.Left = null;
+            root.Parrent = null;
+            Insert(root, tree);
+        }
+
+        public void BigLeftRotation(Node root)
+        {
+            if (root.Right.BalanceFactor > BalanceFactor)
+            {
+                SmallRightRotation(root.Right);
+            }
+            if (root.Right.BalanceFactor < (-1 * BalanceFactor))
+            {
+                SmallLeftRotation(root.Right);
+            }
+            SmallLeftRotation(root);
+        }
+        public void BigRightRotation(Node root)
+        {
+            if (root.Left.BalanceFactor > BalanceFactor)
+            {
+                SmallRightRotation(root.Left);
+            }
+            if (root.Left.BalanceFactor < (-1 * BalanceFactor))
+            {
+                SmallLeftRotation(root.Left);
+            }
+            SmallRightRotation(root);
+        }
+        public void Rebalance(Node node)
+        {
+            if (node == null) return;
+            if (node.Right == null && node.Left == null) return;
+
+            if (node != null)
+            {
+                if (node.Right != null)
+                    Rebalance(node.Right);
+
+                if (node.Left != null)
+                    Rebalance(node.Left);
+            }
+            if (Math.Abs(node.BalanceFactor) > BalanceFactor)
+            {
+                BigRotBalanceTree(node);
+            }
+        }
+        public void BigRotBalanceTree(Node node)
+        {
+            if (Math.Abs(node.BalanceFactor) > BalanceFactor)
+            {
+                if (node.BalanceFactor > BalanceFactor)
+                    BigRightRotation(node);
+
+                if (node.BalanceFactor < (-1 * BalanceFactor))
+                    BigLeftRotation(node);
+            }
+        }
+
+        public Node maxUnBalanceRoot { get; private set; }
+        public void MaxUnBalanceRoot(Node node)
+        {
+            if (node.Right == null && node.Left == null)
+                return;
+
+            if (node != null)
+            {
+                if (node.Right != null)
+                    MaxUnBalanceRoot(node.Right);
+
+                if (node.Left != null)
+                    MaxUnBalanceRoot(node.Left);
+            }
+
+            if (Math.Abs(node.BalanceFactor) > Math.Abs(maxUnBalanceRoot.BalanceFactor))
+                maxUnBalanceRoot = node;
+        }
+
+
+        List<Node> unbalanceRoot = new List<Node>();
+        public List<Node> GetUnbalanceRoot()
+        {
+            unbalanceRoot = new List<Node>();
+            SearchRebalanceOut(_root);
+            return unbalanceRoot;
+        }
+
+        void SearchRebalanceOut(Node node)
+        {
+            if (node.Right == null && node.Left == null)
+                return;
+
+            if (node != null)
+            {
+                if (node.Right != null)
+                    SearchRebalanceOut(node.Right);
+
+                if (node.Left != null)
+                    SearchRebalanceOut(node.Left);
+            }
+            if (Math.Abs(node.BalanceFactor) > this.BalanceFactor)
+            {
+                unbalanceRoot.Add(node);
+            }
+        }
+
+    }
+}
--- BinaryTrees/Program.cs
+++ BinaryTrees/Program.cs
@@ -5,7 +5,18 @@ static class Programm
 {
     public static void Main()
     {
-        int N = 30000;
+        UnOrdered();
+        Ordered();
+        AVLOrderedAndBalance();
+        AVLNonOrderedAndBalance();
+    }
+
+
+    static void UnOrdered()
+    {
+        int N = 10000;
+        Console.WriteLine();
+        Console.WriteLine($"ТЕСТ ПРОСТОГО ДЕРЕВА С {N} НЕУПОРЯДОЧЕННЫМИ ЧИСЛАМИ");
         BinaryTrees.SimpleTrees simpleTreesRnd = new BinaryTrees.SimpleTrees(N);
 
         int[] rndNum = GetRandomNumbers(N);
@@ -17,7 +28,7 @@ static class Programm
             simpleTreesRnd.Insert(rndNum[i]);
         }
         sw.Stop();
-        Console.WriteLine($"Добавление в дерево неупорядоченных чисел от 0 до {N} занимает: {sw.ElapsedMilliseconds} мс.");
+        Console.WriteLine($"Добавление в дерево неупорядоченных чисел от 0 до {N} занимает: {sw.ElapsedTicks * 100} нс.");
 
         sw = new Stopwatch();
         sw.Start();
@@ -26,7 +37,7 @@ static class Programm
             bool searh = simpleTreesRnd.Search(random.Next(1, int.MaxValue));
         }
         sw.Stop();
-        Console.WriteLine($"Поиск в неупорядоченном дереве {N / 10} случайных чисел занимает: {sw.ElapsedMilliseconds} мс.");
+        Console.WriteLine($"Поиск в неупорядоченном дереве {N / 10} случайных чисел занимает: {sw.ElapsedTicks * 100} нс.");
 
         sw = new Stopwatch();
         sw.Start();
@@ -35,38 +46,146 @@ static class Programm
             simpleTreesRnd.Remove(random.Next(1, int.MaxValue));
         }
         sw.Stop();
-        Console.WriteLine($"Удаление в неупорядоченном дереве {N / 10} случайных чисел занимает: {sw.ElapsedMilliseconds} мс.");
-
+        Console.WriteLine($"Удаление в неупорядоченном дереве {N / 10} случайных чисел занимает: {sw.ElapsedTicks * 100} нс.");
+    }
+    static void Ordered()
+    {
+        int N = 10000;
         Console.WriteLine();
-
+        Console.WriteLine($"ТЕСТ ПРОСТОГО ДЕРЕВА С {N} УПОРЯДОЧЕННЫМИ ЧИСЛАМИ");
+        Stopwatch sw = new Stopwatch();
         BinaryTrees.SimpleTrees simpleTreesOrder = new BinaryTrees.SimpleTrees(N);
         int[] orderNum = GetOrderNumbers(N);
         sw = new Stopwatch();
         sw.Start();
         for (int i = 0; i < N; i++)
         {
-          simpleTreesOrder.Insert(orderNum[i]);
+            simpleTreesOrder.Insert(orderNum[i]);
         }
         sw.Stop();
-        Console.WriteLine($"Добавление в дерево упорядоченных по возрастанию чисел от 0 до {N} занимает: {sw.ElapsedMilliseconds} мс.");
+        Console.WriteLine($"Добавление в дерево упорядоченных по возрастанию чисел от 0 до {N} занимает: {sw.ElapsedTicks * 100} нс.");
 
         sw = new Stopwatch();
         sw.Start();
-        for (int i = 0; i < N; i++)
+        for (int i = 0; i < N / 10; i++)
         {
-           bool searh = simpleTreesOrder.Search(random.Next(0, N));
+            bool searh = simpleTreesOrder.Search(random.Next(0, N));
         }
         sw.Stop();
-        Console.WriteLine($"Поиск в упорядоченном дереве {N / 10} случайных чисел занимает: {sw.ElapsedMilliseconds} мс.");
+        Console.WriteLine($"Поиск в упорядоченном дереве {N / 10} случайных чисел занимает: {sw.ElapsedTicks * 100} нс.");
 
         sw = new Stopwatch();
         sw.Start();
-        for (int i = 0; i < N; i++)
+        for (int i = 0; i < N / 10; i++)
         {
             simpleTreesOrder.Remove(random.Next(0, N));
         }
         sw.Stop();
-        Console.WriteLine($"Удаление в упорядоченном дереве {N / 10} случайных чисел занимает: {sw.ElapsedMilliseconds} мс.");
+        Console.WriteLine($"Удаление в упорядоченном дереве {N / 10} случайных чисел занимает: {sw.ElapsedTicks * 100} нс.");
+    }
+    static void AVLOrderedAndBalance()
+    {
+        int N = 10000;
+        Console.WriteLine();
+        Console.WriteLine($"ТЕСТ АВЛ ДЕРЕВА С {N} УПОРЯДОЧЕННЫМИ ЧИСЛАМИ");
+        //Формируем массив упорядоченнных по убыванию чисел
+        int[] orderNum = GetOrderNumbers(N);
+        //Создаем двоичное дерево АВЛ с корнем 10000
+        AVLTrees aVLTrees = new AVLTrees(0);
+
+        Stopwatch sw = new Stopwatch();
+        sw = new Stopwatch();
+        sw.Start();
+        for (int i = 0; i < N; i++)
+        {
+            aVLTrees.Insert(orderNum[i]);
+        }
+        sw.Stop();
+        Console.WriteLine($"Добавление в АВЛ дерево упорядоченных по возрастанию чисел от 0 до {N} занимает: {sw.ElapsedTicks * 100} нс.");
+
+        sw = new Stopwatch();
+        sw.Start();
+        for (int i = 0; i < N / 10; i++)
+        {
+            bool searh = aVLTrees.Search(random.Next(0, N));
+        }
+        sw.Stop();
+        Console.WriteLine($"Поиск в несбалансированном АВЛ дереве {N / 10} случайных чисел занимает: {sw.ElapsedTicks * 100} нс.");
+
+        //Балансируем правое поддерево
+        aVLTrees.BalanceFactor = 1; //Устанавливаем фактор балансировки
+        sw = new Stopwatch();
+        sw.Start();
+        Console.WriteLine($"Балансировка АВЛ дерева. Высота дерева до балансировки {aVLTrees.Root.Height}.");
+        aVLTrees.MaxUnBalanceRoot(aVLTrees.Root.Right);
+        while (Math.Abs(aVLTrees.maxUnBalanceRoot.BalanceFactor) > aVLTrees.BalanceFactor)
+        {
+            aVLTrees.Rebalance(aVLTrees.maxUnBalanceRoot);
+            aVLTrees.MaxUnBalanceRoot(aVLTrees.Root.Right);
+        }
+        sw.Stop();
+        Console.WriteLine($"Высота дерева после балансировки {aVLTrees.Root.Height}. Время балансировки: {sw.ElapsedTicks * 100} нс.");
+
+        sw = new Stopwatch();
+        sw.Start();
+        for (int i = 0; i < N / 10; i++)
+        {
+            bool searh = aVLTrees.Search(random.Next(0, N));
+        }
+        sw.Stop();
+        Console.WriteLine($"Поиск в сбалансированном АВЛ дереве {N / 10} случайных чисел занимает: {sw.ElapsedTicks * 100} нс.");
+    }
+    static void AVLNonOrderedAndBalance()
+    {
+        int N = 10000;
+        Console.WriteLine();
+        Console.WriteLine($"ТЕСТ АВЛ ДЕРЕВА С {N} НЕУПОРЯДОЧЕННЫМИ ЧИСЛАМИ");
+        //Формируем массив упорядоченнных по убыванию чисел
+        int[] rndNums = GetRandomNumbers(N);
+        //Создаем двоичное дерево АВЛ с корнем 10000
+        AVLTrees aVLTrees = new AVLTrees(0);
+
+        Stopwatch sw = new Stopwatch();
+        sw = new Stopwatch();
+        sw.Start();
+        for (int i = 0; i < N; i++)
+        {
+            aVLTrees.Insert(rndNums[i]);
+        }
+        sw.Stop();
+        Console.WriteLine($"Добавление в АВЛ дерево случайных числе от 0 до {N} занимает: {sw.ElapsedTicks * 100} нс.");
+
+        sw = new Stopwatch();
+        sw.Start();
+        for (int i = 0; i < N / 10; i++)
+        {
+            bool searh = aVLTrees.Search(random.Next(0, N));
+        }
+        sw.Stop();
+        Console.WriteLine($"Поиск в несбалансированном АВЛ дереве {N / 10} случайных чисел занимает: {sw.ElapsedTicks * 100} нс.");
+
+        //Балансируем правое поддерево
+        aVLTrees.BalanceFactor = 2; //Устанавливаем фактор балансировки
+        sw = new Stopwatch();
+        sw.Start();
+        Console.WriteLine($"Балансировка АВЛ дерева. Высота дерева до балансировки {aVLTrees.Root.Height}.");
+        aVLTrees.MaxUnBalanceRoot(aVLTrees.Root.Right);
+        while (Math.Abs(aVLTrees.maxUnBalanceRoot.BalanceFactor) > aVLTrees.BalanceFactor)
+        {
+            aVLTrees.Rebalance(aVLTrees.maxUnBalanceRoot);
+            aVLTrees.MaxUnBalanceRoot(aVLTrees.Root.Right);
+        }
+        sw.Stop();
+        Console.WriteLine($"Высота дерева после балансировки {aVLTrees.Root.Height}. Время балансировки: {sw.ElapsedTicks * 100} нс.");
+
+        sw = new Stopwatch();
+        sw.Start();
+        for (int i = 0; i < N / 10; i++)
+        {
+            bool searh = aVLTrees.Search(random.Next(0, N));
+        }
+        sw.Stop();
+        Console.WriteLine($"Поиск в сбалансированном АВЛ дереве {N / 10} случайных чисел занимает: {sw.ElapsedTicks * 100} нс.");
     }
 
 
